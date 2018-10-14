8ab6b751fde168b43d12980f590e442fa8b4bdd3
Kirk Byers <ktbyers@twb-tech.com>
2018-10-14 10:16:47 -0700

    Removing commented lines

--- tests/mock_device.py
+++ tests/mock_device.py
@@ -15,9 +15,3 @@ mock_methods = {"write_channel": write_channel, "read_channel": read_channel}
 
 def ClassFactory(BaseClass):
     return type("MockNetmiko", (BaseClass,), mock_methods)
-
-
-# DynamicClass = ConnectHandler(host='cisco1.lasthop.ip', username='admin', password='foo', device_type='cisco_ios', mock=True)
-
-# MockNetmiko = type("MockNetmiko", (CiscoIosSSH,), mock_methods)
-# a = MockNetmiko(host='cisco1.lasthop.io', username='pyclass', password='88newclass', device_type='cisco_ios', mock=True)
